632ee2f66f26da3b4ed9e70ee08ac463342c6cb5
unknown <aayu.shah711@gmail.com>
2020-09-19 23:48:26 +0530

    CRUD application

---
+++ DS/logn.js
@@ -0,0 +1,27 @@
+// O(log(n))
+function runProgram(input) {
+	let count = 0;
+	// let n = 8
+	// let i = n;
+	let a = 0;
+
+	// log(n)
+	// for (var i = 10; i > 1; i /= 2) {
+	// 	console.log(i);
+	// }
+
+	// log(n)
+	for (var i = 10; i > 1; i /= 10) {
+		console.log(i);
+	}
+
+	// for (var i = 1; i < 10; i *= 2) {
+	// 	console.log(i);
+	// }
+	// while (Math.floor(i) > 0) {
+	// 	a += i;
+	// 	i = i / 2;
+	// }
+}
+
+runProgram(10);
